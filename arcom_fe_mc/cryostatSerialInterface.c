5d5dfa634cddcbc87d7b19d53f438cb4358445dc
Morgan McLeod <mmcleod@nrao.edu>
2014-05-07 17:35:25 -0400

    Cryostat: added debugging option to supress errors.

--- arcom_fe_mc/cryostatSerialInterface.c
+++ arcom_fe_mc/cryostatSerialInterface.c
@@ -213,8 +213,12 @@ static int getCryoAnalogMonitor(void){
 
                 /* Increase the retries counter and if we tried too many times, return error. */
                 if(++retries>CRYO_ADC_MAX_RETRIES){
-                    storeError(ERR_CRYO_SERIAL,
-                               0x01); // Error 0x01 -> Too many retries waiting for ADC_READY
+
+                    #ifndef NO_STOREERROR_CRYOSTAT
+                        // define this symbol in debug.h when debugging with no cryostat M&C module.
+                        storeError(ERR_CRYO_SERIAL,
+                                   0x01); // Error 0x01 -> Too many retries waiting for ADC_READY
+                    #endif
                     retries=0;
 
                     /* Reset state */
